9dae5b6b559c4b435e558099800414338ed45eda
Joe Badaczewski <jbadaczewski@softwriters.com>
2020-12-13 23:06:54 -0500

    Removing clutter

--- packages/downloader/__tests__/downloader.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const downloader = require('..');
-
-describe('downloader', () => {
-    it('needs tests');
-});
--- packages/downloader/lib/downloader.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = downloader;
-
-function downloader() {
-    // TODO
-}
